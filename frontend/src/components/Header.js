583eba7e23aef36f9e7c8c31c189c3221786d3b5
omercha <144559597+omercha@users.noreply.github.com>
2025-09-05 21:28:05 +0100

    Added header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="site-header">
+      <div className="header-content">
+        <button className="hamburger-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <nav className="nav-buttons">
+          <a href="/">Home</a>
+          <a href="/recipes">Recipes</a>
+          <a href="/tracker">Tracker</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
