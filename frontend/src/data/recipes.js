6fd15bf6530b7b8a6e85de8b355c96e91ee0941f
omercha <144559597+omercha@users.noreply.github.com>
2025-09-05 16:57:13 +0100

    Temporarily hardcoded recipe information

---
+++ frontend/src/data/recipes.js
@@ -0,0 +1,67 @@
+const recipes = [
+  {
+    id: 1,
+    name: 'Mushroom Skewer',
+    ingredients: ['Any mushroom'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/1/1d/Mushroom_Skewer_-_TotK_icon.png'
+  },
+  {
+    id: 2,
+    name: 'Meat and Mushroom Skewer',
+    ingredients: ['Any mushroom', 'Any meat'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/e/ed/Meat_and_Mushroom_Skewer_-_TotK_icon.png'
+  },
+  {
+    id: 3,
+    name: 'Fish and Mushroom Skewer',
+    ingredients: ['Any fish', 'Any mushroom'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/3/33/Fish_and_Mushroom_Skewer_-_TotK_icon.png'
+  },
+  {
+    id: 4,
+    name: 'Meat Skewer',
+    ingredients: ['Any meat'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/7/78/Meat_Skewer_-_TotK_icon.png'
+  },
+  {
+    id: 5,
+    name: 'Fish Skewer',
+    ingredients: ['Any fish'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/7/7d/Fish_Skewer_-_TotK_icon.png'
+  },
+  {
+    id: 6,
+    name: 'Seafood Skewer',
+    ingredients: ['Any snail or crab'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/b/b8/Seafood_Skewer_-_TotK_icon.png'
+  },
+  {
+    id: 7,
+    name: 'Copious Meat Skewers',
+    ingredients: ['Any four different meat'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/6/65/Copious_Meat_Skewers_-_TotK_icon.png'
+  },
+  {
+    id: 8,
+    name: 'Copious Fish Skewers',
+    ingredients: ['Any four different fish'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/0/03/Copious_Seafood_Skewers_-_TotK_icon.png'
+  },
+  {
+    id: 9,
+    name: 'Steamed Fruit',
+    ingredients: ['Any fruit (excluding Hylian tomato)', 'Any vegetable, flower, or herb'],
+    effect: null,
+    image: 'https://www.zeldadungeon.net/wiki/images/9/93/Steamed_Fruit_-_TotK_icon.png'
+  }
+];
+
+export default recipes;
