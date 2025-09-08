f74602c6845cd2f887f07ac37b349abd9a67ec0b
omercha <144559597+omercha@users.noreply.github.com>
2025-09-08 16:17:58 +0100

    Added tracker page

---
+++ frontend/src/pages/Tracker.js
@@ -0,0 +1,10 @@
+function Tracker() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>Recipe Tracker</h2>
+      <p>Track completed recipes (coming soon!).</p>
+    </div>
+  );
+}
+
+export default Tracker;
