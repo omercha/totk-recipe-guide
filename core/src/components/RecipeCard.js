a360efe4713ef8265265ef33e13c4d669231901c
omercha <144559597+omercha@users.noreply.github.com>
2025-09-11 19:18:15 +0100

    Updated background colour for each recipe based on its cooking method

--- core/src/components/RecipeCard.js
+++ core/src/components/RecipeCard.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './RecipeCard.css';
 
+const methodColours = {
+  extreme_heat: '#fce5cd',
+  extreme_cold: '#d6f8fb',
+  hot_spring: '#d9d9d9',
+};
+
 const RecipeCard = ({ recipe }) => {
+  const imageStyles = {
+    backgroundColor: methodColours[recipe.method],
+  };
   return (
     <div className="recipe-card">
-      <img src={recipe.image} alt={recipe.name} className="recipe-card-image" />
+      <div className="recipe-card-image-container" style={imageStyles}>
+        <img src={recipe.image} alt={recipe.name} className="recipe-card-image" />
+      </div>
       <div className="recipe-card-body">
-        <h3 className="recipe-card-title">{recipe.name}</h3>
+        <h3 className="recipe-card-title">
+          <span>{recipe.name}</span>
+          <span>#{recipe.id}</span>
+        </h3>
         <ul className="recipe-card-ingredients">
           {recipe.ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
